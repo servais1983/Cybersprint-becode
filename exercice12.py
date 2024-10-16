1445b7dffa380530c47870951b51ab1445070e84
steve servais <stservais0409@gmail.com>
2024-10-16 11:21:05 +0200

    ajout exercice 12 final

---
+++ exercice12.py
@@ -0,0 +1,10 @@
+translation = {}
+translation["Apple"] = "pomme"
+translation["Dog"] = "chien"
+translation["House"] = "maison"
+translation["Cloud"] = "nuage"
+translation["Car"] = "voiture"
+print(translation)
+definition = {}
+definition["lonely day"] = "journée de solitude"
+print(definition)

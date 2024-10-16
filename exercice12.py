7e80bf2e93de2bde36279434cf884dd4f8136468
steve servais <stservais0409@gmail.com>
2024-10-16 10:10:37 +0200

    ajout exercice 12

---
+++ exercice12.py
@@ -0,0 +1,11 @@
+translation = {}
+translation["Apple"] = "pomme"
+translation["Dog"] = "chien"
+translation["House"] = "maison"
+translation["Cloud"] = "nuage"
+translation["Car"] = "voiture"
+print(translation["Apple"])
+print(translation["Dog"])
+print(translation["House"])
+print(translation["Cloud"])
+print(translation["Car"])

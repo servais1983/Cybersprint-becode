29bebb633ea02dbb8d042e98dea063c445aa9c40
steve servais <stservais0409@gmail.com>
2024-10-17 10:20:33 +0200

    ajout exercice 26

---
+++ exercice26.py
@@ -0,0 +1,13 @@
+string = "toto"
+number = 10
+unicode_string = string  
+float_number = float(number)
+
+print(unicode_string) 
+print(float_number)
+
+reversed_string = string[::-1]
+reversed_number = int(str(number)[::-1])
+
+print(reversed_string)  
+print(reversed_number)
